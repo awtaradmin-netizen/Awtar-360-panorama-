be734261d8cd05332263017c47d12c824c936843
awtaradmin-netizen <awtar.admin@gmail.com>
2026-08-13 16:45:34 +0300

    initial upload
    360 panorama website

---
+++ data.js
@@ -0,0 +1,322 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-exit",
+      "name": "Exit",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3000,
+      "initialViewParameters": {
+        "yaw": 0.014639441964778399,
+        "pitch": 0.09436127400475769,
+        "fov": 1.325599857056214
+      },
+      "linkHotspots": [
+        {
+          "yaw": -3.129430257760095,
+          "pitch": 0.04164565041207169,
+          "rotation": 0,
+          "target": "1-midel"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-midel",
+      "name": "midel",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3000,
+      "initialViewParameters": {
+        "yaw": 0.3667246764777836,
+        "pitch": -0.10499565695722879,
+        "fov": 1.325599857056214
+      },
+      "linkHotspots": [
+        {
+          "yaw": -3.1239725718322706,
+          "pitch": 0.05653342478886003,
+          "rotation": 6.283185307179586,
+          "target": "2-corner"
+        },
+        {
+          "yaw": 0.02451343526733929,
+          "pitch": 0.1711562231391106,
+          "rotation": 0,
+          "target": "1-midel"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-corner",
+      "name": "corner",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3000,
+      "initialViewParameters": {
+        "yaw": -0.901856531607276,
+        "pitch": 0.0700977393502491,
+        "fov": 1.325599857056214
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.03068709025634675,
+          "pitch": 0.09975758946406899,
+          "rotation": 0,
+          "target": "1-midel"
+        },
+        {
+          "yaw": -1.7715531158608435,
+          "pitch": 0.09164366497065046,
+          "rotation": 0,
+          "target": "3-"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-",
+      "name": "الحديقة",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3000,
+      "initialViewParameters": {
+        "yaw": -0.7332416649201736,
+        "pitch": 0.07685291461275412,
+        "fov": 1.325599857056214
+      },
+      "linkHotspots": [
+        {
+          "yaw": -3.091107441434028,
+          "pitch": 0.11047839652802338,
+          "rotation": 7.853981633974483,
+          "target": "4---"
+        },
+        {
+          "yaw": 0.11251929315168141,
+          "pitch": 0.09074487837055578,
+          "rotation": 0,
+          "target": "2-corner"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4---",
+      "name": "زاوية المبنا الثانية",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3000,
+      "initialViewParameters": {
+        "yaw": -1.4368413868713912,
+        "pitch": 0.1335462707869901,
+        "fov": 1.325599857056214
+      },
+      "linkHotspots": [
+        {
+          "yaw": 3.1109835463369713,
+          "pitch": 0.09045451746141708,
+          "rotation": 6.283185307179586,
+          "target": "5--2"
+        },
+        {
+          "yaw": -1.4368413868713912,
+          "pitch": 0.1335462707869901,
+          "rotation": 0,
+          "target": "3-"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "5--2",
+      "name": "عمارة 2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3000,
+      "initialViewParameters": {
+        "yaw": 0.1780948216320617,
+        "pitch": 0.022527186345426387,
+        "fov": 1.325599857056214
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    },
+    {
+      "id": "6--",
+      "name": "بوابة الدخول",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3000,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.019346102654976605,
+          "pitch": 0.14983039762592476,
+          "rotation": 0,
+          "target": "5--2"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
